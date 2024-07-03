306b298e6981889c7de8750f4826c6b8623b5a7f
chino0522 <baekjinho0522@gmail.com>
2024-07-03 12:34:53 -0700

    Deleted database and created posts folder to save markdown files for blog postings

--- app/api/hooks/getDatabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export default async function getDatabase() {
-    const supabaseUrl = process.env.SUPABASE_URL
-    const supabaseKey = process.env.SUPABASE_KEY
-    const supabase = createClient(supabaseUrl || "", supabaseKey || "")
-    return supabase
-}
