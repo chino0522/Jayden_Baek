8f22354741de1adde7f9857ebe279b92af65cf52
chino0522 <baekjinho0522@gmail.com>
2024-07-14 21:42:11 -0700

    Removed animation of being disappeared when scrolled down since it distracts more and found another bug

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,7 +2,6 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 
 type HeaderItemProps = {
     itemName: string;
@@ -10,36 +9,6 @@ type HeaderItemProps = {
 }
 
 const Header = () => {
-    const [isScrollingDown, setIsScrollingDown] = useState(false);
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-            }
-
-            const newTimeoutId = setTimeout(() => {
-                if (window.scrollY > lastScrollY) {
-                    setIsScrollingDown(true);
-                } else {
-                    setIsScrollingDown(false);
-                }
-                setLastScrollY(window.scrollY);
-            }, 100);
-
-            setTimeoutId(newTimeoutId);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-            }
-        };
-    }, [lastScrollY, timeoutId]);
 
     const HeaderItem = ({ itemName, pathName }: HeaderItemProps) => {
         const currentPathname = usePathname();
@@ -48,7 +17,7 @@ const Header = () => {
         const bgColor = isActive ? 'bg-black' : 'bg-transparent';
 
         return (
-            <div className={`${textColor} group relative my-4 mx-3 px-4 py-2 md:px-10 flex items-center justify-center flex-grow-0 flex-shrink-0 cursor-pointer transition duration-300 ease-in-out w-24 md:w-32 text-center rounded-2xl overflow-hidden`}>
+            <div className={`${textColor} group relative my-2 mt-3 md:my-4 mx-3 px-4 py-2 md:px-10 flex items-center justify-center flex-grow-0 flex-shrink-0 cursor-pointer transition duration-300 ease-in-out w-24 md:w-32 text-center rounded-2xl overflow-hidden`}>
                 <div className={`absolute inset-0 ${bgColor} transition-all duration-300 ease-in-out`}></div>
                 <div className="relative z-10">
                     {itemName}
@@ -58,7 +27,7 @@ const Header = () => {
     };
 
     return (
-        <div className={`w-full h-full bg-white bg-opacity-95 text-center sticky top-0 z-10 flex flex-wrap content-evenly justify-evenly transition-transform duration-500 ${isScrollingDown ? '-translate-y-full' : 'translate-y-0'}`}>
+        <div className={`w-full h-full bg-white bg-opacity-95 text-center sticky top-0 z-10 flex flex-wrap content-evenly justify-evenly transition-transform duration-500 `}>
             <div className="flex shrink-1 justify-evenly items-center">
                 <Link href="/"><HeaderItem itemName="Profile" pathName='/' /></Link>
                 <Link href="/blog"><HeaderItem itemName="Blog" pathName='/blog' /></Link>
